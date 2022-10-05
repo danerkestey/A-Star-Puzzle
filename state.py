cf64752ef237e9059fe8f42625802381c4e2ac0a
saifaldin14 <saifaldindeveloper@gmail.com>
2022-10-05 13:28:18 -0400

    Updated move functions to use the heuristics

--- state.py
+++ state.py
@@ -2,46 +2,69 @@ from puzzle.generate import *
 
 
 class State:
-    def __init__(self, size):
+    def __init__(self, size, heuristic):
         self.size = size
         self.currentState = generateValidRandomMatrix(self.size)
         self.hash = generateHashMatrix(self.currentState)
         self.i1 = getEmptySpacePosition(self.hash)[0]
         self.j1 = getEmptySpacePosition(self.hash)[1]
+        self.heuristic = heuristic
 
     def swap(self, i2, j2):
-        temp = self.currentState[self.i1][self.j1]
-        self.currentState[self.i1][self.j1] = self.currentState[i2][j2]
-        self.currentState[i2][j2] = temp
+        tempMat = self.currentState
+        temp = tempMat[self.i1][self.j1]
+        tempMat[self.i1][self.j1] = tempMat[i2][j2]
+        tempMat[i2][j2] = temp
+        return tempMat
 
     def moveUp(self):
         print(self.currentState)
         i2, j2 = self.i1 + 1, self.j1
+        temp = self.currentState
+        dp = self.heuristic(temp)
         if i2 < len(self.currentState):
-            self.swap(i2, j2)
-        print(self.currentState)
+            temp = self.swap(i2, j2)
+            dp = self.heuristic(temp)
+        else:
+            dp = float('inf')
+        return [dp, temp]
 
     def moveDown(self):
         print(self.currentState)
         i2, j2 = self.i1 - 1, self.j1
+        temp = self.currentState
+        dp = self.heuristic(temp)
         if i2 >= 0:
-            self.swap(i2, j2)
-        print(self.currentState)
+            temp = self.swap(i2, j2)
+            dp = self.heuristic(temp)
+        else:
+            dp = float('inf')
+        return [dp, temp]
 
     def moveLeft(self):
         print(self.currentState)
         i2, j2 = self.i1, self.j1 - 1
+        temp = self.currentState
+        dp = self.heuristic(temp)
         if j2 >= 0:
-            self.swap(i2, j2)
-        print(self.currentState)
+            temp = self.swap(i2, j2)
+            dp = self.heuristic(temp)
+        else:
+            dp = float('inf')
+        return [dp, temp]
 
     def moveRight(self):
         print(self.currentState)
         i2, j2 = self.i1, self.j1 + 1
+        temp = self.currentState
+        dp = self.heuristic(temp)
         if j2 < len(self.currentState[0]):
-            self.swap(i2, j2)
-        print(self.currentState)
+            temp = self.swap(i2, j2)
+            dp = self.heuristic(temp)
+        else:
+            dp = float('inf')
+        return [dp, temp]
 
 
-state = State(8)
-state.moveDown()
+state = State(8, disorderParameter)
+print(state.moveUp())
