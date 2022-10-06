a9749e72e5927cd8514c089ad7b78798e9058df4
saifaldin14 <saifaldindeveloper@gmail.com>
2022-10-06 12:32:25 -0400

    Trying to write solve, it's still not working

--- state.py
+++ state.py
@@ -5,74 +5,138 @@ from utils.heuristics import *
 class State:
     def __init__(self, size, heuristic):
         self.size = size
-        self.currentState = generateValidRandomMatrix(self.size)
-        self.hash = generateHashMatrix(self.currentState)
-        self.i1 = getEmptySpacePosition(self.hash)[0]
-        self.j1 = getEmptySpacePosition(self.hash)[1]
-        self.heuristic = heuristic
+        self.currentState = [generateValidRandomMatrix(self.size)]
+        self.heuristic = heuristic  # h(n)
+        self.depth = 0  # g(n)
         self.solved = generateSolvedMatrix(self.size)
         self.steps = 0
+        self.seenStates = []
 
-    def swap(self, i2, j2):
-        tempMat = self.currentState
-        temp = tempMat[self.i1][self.j1]
-        tempMat[self.i1][self.j1] = tempMat[i2][j2]
+    def getUniqueIdentifier(self, state, depth):
+        state1D = list(chain.from_iterable(state))
+        dp = disorderParameter(state)
+
+        uniqueStr = "".join(str(x) for x in state1D)
+        uniqueStr += str(depth)
+        uniqueStr += str(dp)
+
+        return uniqueStr
+
+    def getRequiredValues(self, state):
+        hash = generateHashMatrix(state)
+        i1 = getEmptySpacePosition(hash)[0]
+        j1 = getEmptySpacePosition(hash)[1]
+
+        return i1, j1
+
+    def swap(self, state, i1, j1, i2, j2):
+        tempMat = state
+        temp = tempMat[i1][j1]
+        tempMat[i1][j1] = tempMat[i2][j2]
         tempMat[i2][j2] = temp
         return tempMat
 
-    def moveUp(self):
-        i2, j2 = self.i1 + 1, self.j1
-        temp = self.currentState
+    def moveUp(self, state, depth):
+        i1, j1 = self.getRequiredValues(state)
+        i2, j2 = i1 + 1, j1
+        temp = state
         h = self.heuristic(temp, self.size)
-        if i2 < len(self.currentState):
-            temp = self.swap(i2, j2)
+        g = depth + 1
+        if i2 < len(state):
+            temp = self.swap(state, i1, j1, i2, j2)
             h = self.heuristic(temp, self.size)
         else:
             h = float('inf')
-        return [h, temp]
 
-    def moveDown(self):
-        i2, j2 = self.i1 - 1, self.j1
-        temp = self.currentState
+        f = g + h
+        return [f, temp]
+
+    def moveDown(self, state, depth):
+        i1, j1 = self.getRequiredValues(state)
+        i2, j2 = i1 - 1, j1
+        temp = state
         h = self.heuristic(temp, self.size)
+        g = depth + 1
         if i2 >= 0:
-            temp = self.swap(i2, j2)
+            temp = self.swap(state, i1, j1, i2, j2)
             h = self.heuristic(temp, self.size)
         else:
             h = float('inf')
-        return [h, temp]
 
-    def moveLeft(self):
-        i2, j2 = self.i1, self.j1 - 1
-        temp = self.currentState
+        f = g + h
+        return [f, temp]
+
+    def moveLeft(self, state, depth):
+        i1, j1 = self.getRequiredValues(state)
+        i2, j2 = i1, j1 - 1
+        temp = state
         h = self.heuristic(temp, self.size)
+        g = depth + 1
         if j2 >= 0:
-            temp = self.swap(i2, j2)
+            temp = self.swap(state, i1, j1, i2, j2)
             h = self.heuristic(temp, self.size)
         else:
             h = float('inf')
-        return [h, temp]
 
-    def moveRight(self):
-        i2, j2 = self.i1, self.j1 + 1
-        temp = self.currentState
+        f = g + h
+        return [f, temp]
+
+    def moveRight(self, state, depth):
+        i1, j1 = self.getRequiredValues(state)
+        i2, j2 = i1, j1 + 1
+        temp = state
         h = self.heuristic(temp, self.size)
-        if j2 < len(self.currentState[0]):
-            temp = self.swap(i2, j2)
+        g = depth + 1
+        if j2 < len(state[0]):
+            temp = self.swap(state, i1, j1, i2, j2)
             h = self.heuristic(temp, self.size)
         else:
             h = float('inf')
-        return [h, temp]
+
+        f = g + h
+        return [f, temp]
 
     def solve(self):
-        print(self.currentState)
-        print(self.solved)
-        while self.heuristic(self.currentState, self.size) != 0:
-            print(self.currentState)
-            t1, t2, t3, t3 = self.moveUp(), self.moveDown(), self.moveLeft(), self.moveRight()
-            nextState = sorted([t1, t2, t3, t3])[0]
-            self.steps += 1
-            self.currentState = nextState[1]
+        # print(self.currentState)
+        # print(self.solved)
+        # g = self.depth
+        # state = self.currentState
+        # size = self.size
+        # while self.heuristic(state, size) != 0:
+        #     t1, t2, t3, t3 = self.moveUp(g), self.moveDown(
+        #         g), self.moveLeft(g), self.moveRight(g)
+        #     nextState = sorted([t1, t2, t3, t3])[0]
+        #     # print("State: ", nextState[1], " f value: ", nextState[0])
+        #     print(sorted([t1, t2, t3, t3]))
+        #     self.steps += 1
+        #     g += 1
+        #     state = nextState[1]
+        self.seenStates.append(self.getUniqueIdentifier(
+            self.currentState[0], self.depth))
+        while self.heuristic(self.currentState[0], self.size) != 0:
+            self.depth += 1
+            for state in self.currentState:
+                t1, t2, t3, t4 = self.moveUp(state, self.depth), self.moveDown(
+                    state, self.depth), self.moveLeft(state, self.depth), self.moveRight(state, self.depth)
+                potentialStates = {}
+                fValues = []
+                for t in [t1, t2, t3, t4]:
+                    tStr = self.getUniqueIdentifier(t[1], self.depth)
+                    if tStr not in self.seenStates:
+                        fValues.append(t[0])
+                        potentialStates[t[0]] = t[1]
+                minF = min(fValues)
+                nextState = []
+                for key in potentialStates:
+                    if key == minF:
+                        nextState.append(potentialStates[key])
+                        self.seenStates.append(potentialStates[key])
+                self.currentState = nextState
+                self.steps += 1
+                print(self.currentState)
+
+    def test(self):
+        print(self.heuristic(self.solved, self.size))
 
 
 state = State(8, h1)
