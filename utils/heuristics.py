b548301f11c94031a6371bd5ef80c614bac8c60a
Daner Yasin <77366878+danerkestey@users.noreply.github.com>
2022-10-15 23:52:22 -0400

    formatting

--- utils/heuristics.py
+++ utils/heuristics.py
@@ -29,11 +29,11 @@ def heuristic3(state, solved):
     # This heuristic is the sum of the Eucledean distances of each tile from its goal position
     # returns the sum of the Eucledean distances of each tile from its goal position
 
-    #by converting the array to an np array we can grab the index of a 2d array with ease
+    # by converting the array to an np array we can grab the index of a 2d array with ease
     npStateArray = np.array(state)
     npSolvedArray = np.array(solved)
 
-    euclidDistances = []
+    eucledDistances = []
 
     for col in range(len(npStateArray[0])):
         for row in range(len(npStateArray[0])):
@@ -42,11 +42,11 @@ def heuristic3(state, solved):
                 goalPosition = np.argwhere(npSolvedArray == npStateArray[col][row])
 
                 #formula sqrt((colcurr - colgoal)^2 + (rowcurr - rowgoal)^2)
-                euclidDistances.append(np.sqrt((currPosition[0][0] - goalPosition[0][0])**2 + (currPosition[0][1] - goalPosition[0][1])**2))
+                eucledDistances.append(np.sqrt((currPosition[0][0] - goalPosition[0][0])**2 + (currPosition[0][1] - goalPosition[0][1])**2))
 
-    euclidSum = sum(euclidDistances)
+    eucledSum = sum(eucledDistances)
 
-    return euclidSum
+    return eucledSum
 
 
 #### testing below ####
