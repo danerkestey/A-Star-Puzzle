df7c11b9c9868826bb34f7223f4f9cbe2482d990
Daner Yasin <77366878+danerkestey@users.noreply.github.com>
2022-10-15 23:52:07 -0400

    added sum of eucled distance heuristic

--- utils/heuristics.py
+++ utils/heuristics.py
@@ -1,8 +1,6 @@
 import numpy as np
 
-
 def getFValue(state, solved):
-
     #by converting the array to an np array we can grab the index of a 2d array with ease
     npStateArray = np.array(state)
     npSolvedArray = np.array(solved)
@@ -25,9 +23,31 @@ def getFValue(state, solved):
 
     manhattanValue = sum(manhattanDistances)
     return manhattanValue + misplacedTiles
+
     
-def heuristic3():
-    return
+def heuristic3(state, solved):
+    # This heuristic is the sum of the Eucledean distances of each tile from its goal position
+    # returns the sum of the Eucledean distances of each tile from its goal position
+
+    #by converting the array to an np array we can grab the index of a 2d array with ease
+    npStateArray = np.array(state)
+    npSolvedArray = np.array(solved)
+
+    euclidDistances = []
+
+    for col in range(len(npStateArray[0])):
+        for row in range(len(npStateArray[0])):
+            if npStateArray[col][row] != 0:
+                currPosition = np.argwhere(npStateArray == npStateArray[col][row])
+                goalPosition = np.argwhere(npSolvedArray == npStateArray[col][row])
+
+                #formula sqrt((colcurr - colgoal)^2 + (rowcurr - rowgoal)^2)
+                euclidDistances.append(np.sqrt((currPosition[0][0] - goalPosition[0][0])**2 + (currPosition[0][1] - goalPosition[0][1])**2))
+
+    euclidSum = sum(euclidDistances)
+
+    return euclidSum
+
 
 #### testing below ####
 """ testState = [[1, 5, 2],
